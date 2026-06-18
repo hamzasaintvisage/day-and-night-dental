da7defabc0a7aa89e5e263ee6cc51ffbbf67a735
fqpmc824wx-droid <fqpmc824wx@privaterelay.appleid.com>
2026-06-18 23:06:15 +0100

    auto-backup 2026-06-18 23:06

--- src/sections/SmileGallery.jsx
+++ src/sections/SmileGallery.jsx
@@ -6,6 +6,7 @@ const smiles = [
   { image: '/smiles/bonding-patient-2.webp', caption: 'Composite bonding', cat: 'Cosmetic', side: 'night' },
   { image: '/smiles/implant-patient.webp', caption: 'Dental implants', cat: 'Implants', side: 'day' },
   { image: '/smiles/invisalign-patient.webp', caption: 'Invisalign', cat: 'Orthodontics', side: 'night' },
+  { image: '/smiles/featured-smile.webp', caption: 'Porcelain veneers', cat: 'Cosmetic', side: 'day' },
   { image: '/smiles/veneers-patient-2.webp', caption: 'Porcelain veneers', cat: 'Cosmetic', side: 'night' },
 ]
 
